3cf9145fd90394019c175efa811fd336115730fe
erigers <erigersmamoci@gmail.com>
2016-12-18 21:05:16 +0100

    v.0.2.0 - Sass compiled & integrated, App.vue compiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var vuemaker = require('gulp-vuemaker');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  return gulp.src('./src/dev/**/*.+(js|css|html)')
+gulp.task('vuemaker', function() {
+  return gulp.src('./src/dev/**/*.+(js|html)')
     .pipe(vuemaker())
     .pipe(gulp.dest('./src'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('./src/dev/**/*.+(js|css|html)', ['default']);
+gulp.task('sass', function(){
+  return gulp.src('./src/dev/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'))
 });
+
+gulp.task('watch', function(){
+  gulp.watch('./src/dev/**/*.+(js|scss|html)', ['vuemaker', 'sass']);
+});
+
+gulp.task('default',['vuemaker', 'sass']);
